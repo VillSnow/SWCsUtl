28980776ed51a3bc31edf342306678c033f9d8ed
VillSnow <vill.snow@gmail.com>
2019-08-31 12:51:22 +0900

    added Clamp

---
+++ SWCsUtl.Tests/MathToolsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SWCsUtl.Tests {
+	[TestClass]
+	public class MathToolsTests {
+
+		[TestMethod]
+		[DataRow(15, 10, 20, 15)]
+		[DataRow(5, 10, 20, 10)]
+		[DataRow(25, 10, 20, 20)]
+		[DataRow(10, 10, 20, 10)]
+		[DataRow(20, 10, 20, 20)]
+		public void TestClampInt(int x, int min, int max, int expected) {
+			Assert.AreEqual(expected, MathTools.Clamp(x, min, max));
+		}
+	}
+}
---
+++ SWCsUtl/MathTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWCsUtl {
+	public static class MathTools {
+
+		public static T Clamp<T>(T x, T min, T max) where T : IComparable<T> {
+			var c = Comparer<T>.Default;
+			if (c.Compare(min, x) > 0) { return min; }
+			if (c.Compare(x, max) > 0) { return max; }
+			return x;
+		}
+
+	}
+}
